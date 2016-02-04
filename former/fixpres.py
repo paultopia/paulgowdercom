fd77d1ffb3c15d9d6071523a9c08641640e3a691
Paul Gowder <paul.gowder@gmail.com>
2016-02-04 13:18:39 -0600

    scoop pres list out of bad json format start

---
+++ former/fixpres.py
@@ -0,0 +1,19 @@
+import json
+with open('presold.json') as old:
+	oldjson = json.load(old)
+
+
+
+
+def parsepres(prestype):
+	parsed = []
+	for year, paperdict in prestype.iteritems():
+		for title, conflist in paperdict.iteritems():
+			for conference in conflist:
+				parsed.append({"conference": conference, "title": title, "year": year})
+	return parsed
+
+
+print parsepres(oldjson["Presentations"]["Invited Talks"])
+
+# it works, all I need to do now is parse each type, put them all in a dictionary, and save as new json.
