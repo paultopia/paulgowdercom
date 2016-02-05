9f5c28c7e7a9f88a68f234b64ae1c5eafd7a1528
Paul Gowder <paul.gowder@gmail.com>
2016-02-04 22:36:55 -0600

    more css tweaks

--- rendercv.py
+++ rendercv.py
@@ -13,4 +13,3 @@ rendered = env.get_template('cvtemplate.html').render(data)
 with open('newcv.html', 'w') as outfile:
     outfile.write(rendered)
 
-# legal practice all broke, and role on disciplinary service and dates on community service broke
