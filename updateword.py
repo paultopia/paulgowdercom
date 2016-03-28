25297da0c2b3de6c6e6b9660c730047c1e6c2983
Paul Gowder <paul.gowder@gmail.com>
2016-03-28 10:04:50 -0500

    kill word after; comment with tools used for others

--- updateword.py
+++ updateword.py
@@ -1,12 +1,16 @@
 import os, time, glob
+# using this awesome template tool: https://github.com/open-xml-templating/docxtemplater
 os.system('node wordcv.js')
 os.system('launch -p gowdercv.docx')
+# using tools available here: http://stackoverflow.com/a/10221617/4386239
 os.chdir('/users/shared/PDFwriter/pauliglot')
 for x in range(90):
 	time.sleep(1)
 	if glob.glob('*.pdf'):
 		break
 file = glob.glob('*.pdf')[0]
+# kill word using very nice cli here https://github.com/nriley/appswitch/
+os.system('appswitch -qi com.microsoft.Word')
 print file
 
 # fixed
