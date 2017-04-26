3142e9f479436e3fc42637c776ed7ce4562d15a6
Paul Gowder <paul.gowder@gmail.com>
2017-04-26 10:40:48 -0500

    using single-file component successfully

---
+++ revision/src/articlerow.vue
@@ -0,0 +1,27 @@
+<template>
+    <tr v-if="this.infoline">
+        <td>{{ art.title }}</td>
+        <td>{{ art.journal }} {{ art.volume }}<span v-if="art.issue">({{ art.issue }})</span>:{{ art.firstpage }}-{{ art.lastpage }}<span v-if="art.coauthor"> (with {{ art.coauthor }})</span></td>
+        <td>{{ art.year }}</td>
+        <td>
+            <span class="hint--bottom" aria-label="abstract" v-if="art.abstract" v-on:click="abs()"><icon name="file-text"></icon></span>
+            <a class="hint--bottom" aria-label="publisher link" v-if="art.publink" :href="art.publink"><icon name="external-link-square"></icon></a>
+            <a class="hint--bottom" aria-label="alternative download link" v-if="art.altlink" :href="art.altlink"><icon name="download"></icon></a>
+        </td>
+    </tr>
+
+    <tr v-else> <td colspan="3">{{ art.abstract }}</td>
+        <td style="vertical-align: top;"><span class="hint--bottom" aria-label="close abstract" v-on:click="line()"><icon name="times"></icon></span></td>
+    </tr>
+</template>
+
+<script>
+
+ module.exports = {
+     props: ["art"],
+     data: function(){return {infoline: true};},
+     methods: {
+         abs(){this.infoline = false;},
+         line(){this.infoline = true;}
+     }}
+</script>
--- revision/src/main.js
+++ revision/src/main.js
@@ -28,19 +28,22 @@ import 'vue-awesome/icons/times';
 
 // css imports
 
-import '../css/normalize.css'
-import '../css/skeleton.css'
-import '../css/hint.min.css'
-import '../css/custom.css'
+import '../css/normalize.css';
+import '../css/skeleton.css';
+import '../css/hint.min.css';
+import '../css/custom.css';
 
 // image imports
 
 import headshotURL from '../images/headshot.jpg';
 
+// my external components (declared in global vue object below)
+
+import articlerow from './articlerow.vue';
+
 function isArticle(pub){
     return pub.type === "peer review" || pub.type === "law review";
 }
-
 
 function chronThenTitle(a, b){
     if(parseInt(a.year) > parseInt(b.year)) return -1;
@@ -52,10 +55,7 @@ function chronThenTitle(a, b){
 }
 
 
-
 // vue components follow
-
-
 
 Vue.component("big-nav-item", {
     props: ["icon", "word", "toggle"],
@@ -102,37 +102,10 @@ Vue.component("double-little-nav-item", {
     }
 });
 
-Vue.component("articlerow", {
-    props: ["art"],
-    data: function(){return {infoline: true};},
-    methods: {
-        abs(){this.infoline = false;},
-        line(){this.infoline = true;}
-             },
-    template: `<tr v-if="this.infoline">
-<td>{{ art.title }}</td>
-<td>{{ art.journal }} {{ art.volume }}<span v-if="art.issue">({{ art.issue }})</span>:{{ art.firstpage }}-{{ art.lastpage }}<span v-if="art.coauthor"> (with {{ art.coauthor }})</span></td>
-<td>{{ art.year }}</td>
-<td>
-<span class="hint--bottom" aria-label="abstract" v-if="art.abstract" v-on:click="abs()"><icon name="file-text"></icon></span>
-<a class="hint--bottom" aria-label="publisher link" v-if="art.publink" :href="art.publink"><icon name="external-link-square"></icon></a>
-<a class="hint--bottom" aria-label="alternative download link" v-if="art.altlink" :href="art.altlink"><icon name="download"></icon></a>
-</td>
-</tr>
-
-<tr v-else> <td colspan="3">{{ art.abstract }}</td>
-<td style="vertical-align: top;"><span class="hint--bottom" aria-label="close abstract" v-on:click="line()"><icon name="times"></icon></span></td>
-</tr>
-`
-});
-
-
-
-
 function loader(){
     var app = new Vue({
         el: '#app',
-        components: {bio},
+        components: {bio, articlerow},
         data: {publications: pubs,
                headshotURL: headshotURL,
                articles: pubs.filter(isArticle).sort(chronThenTitle),
