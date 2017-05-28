6cdd6574039c4150b0be5e182bddd0f38e59fd1e
Paul Gowder <paul.gowder@gmail.com>
2017-05-28 16:48:06 -0500

    start citation generator

--- revision/src/components/pubs/articlerow.vue
+++ revision/src/components/pubs/articlerow.vue
@@ -4,21 +4,29 @@
         <td>{{ art.journal }} {{ art.volume }}<span v-if="art.issue">({{ art.issue }})</span>:{{ art.firstpage }}-{{ art.lastpage }}<span v-if="art.coauthor"> (with {{ art.coauthor }})</span></td>
         <td>{{ art.year }}</td>
         <td>
-            <span class="hint--bottom" aria-label="abstract" v-if="art.abstract" v-on:click="abs()"><icon name="file-text"></icon></span>
+            <span class="hint--bottom" aria-label="abstract and full citation" v-if="art.abstract" v-on:click="abs()"><icon name="file-text"></icon></span>
             <a class="hint--bottom" aria-label="publisher link" v-if="art.publink" :href="art.publink"><icon name="external-link-square"></icon></a>
             <a class="hint--bottom" aria-label="alternative download link" v-if="art.altlink" :href="art.altlink"><icon name="download"></icon></a>
         </td>
     </tr>
 
-    <tr v-else> <td colspan="3">{{ art.abstract }}</td>
+    <tr v-else> <td colspan="3">
+<p>{{ art.abstract }}</p>
+<p>
+<citation :art="art"></citation></p>
+
+</td>
         <td style="vertical-align: top;"><span class="hint--bottom" aria-label="close abstract" v-on:click="line()"><icon name="times"></icon></span></td>
     </tr>
 </template>
 
 <script>
 
- module.exports = {
+import citation from "./citation.vue";
+
+ export default {
      props: ["art"],
+     components: {citation},
      data: function(){return {infoline: true};},
      methods: {
          abs(){this.infoline = false;},
---
+++ revision/src/components/pubs/citation.vue
@@ -0,0 +1,83 @@
+<template>
+
+<div class="citation">
+
+<p v-if="(art.type == 'peer review' || art.type == 'law review') && citeFormat == 'Chicago'">
+
+<span v-if="art.coauthor">{{authorMaker(art.coauthor)}}</span><span v-else>Gowder, Paul.</span>
+
+Chicago Article
+</p>
+
+<p v-else-if="(art.type == 'peer review' || art.type == 'law review') && citeFormat == 'Bluebook'">
+Bluebook Article
+</p>
+
+<p v-else-if="(art.type == 'peer review' || art.type == 'law review') && citeFormat == 'APA'">
+APA Article
+</p>
+
+<p v-else-if="(art.type == 'peer review' || art.type == 'law review') && citeFormat == 'MLA'">
+MLA Article
+</p>
+
+<p v-else-if="art.type == 'book' && citeFormat == 'Chicago'">
+Chicago Book
+</p>
+
+<p v-else-if="art.type == 'book' && citeFormat == 'Bluebook'">
+Bluebook Book
+</p>
+
+<p v-else-if="art.type == 'book' && citeFormat == 'APA'">
+APA Book
+</p>
+
+<p v-else-if="art.type == 'book' && citeFormat == 'MLA'">
+MLA Book
+</p>
+
+<p v-else-if="art.type == 'chapter' && citeFormat == 'Chicago'">
+Chicago Chapter
+</p>
+
+<p v-else-if="art.type == 'chapter' && citeFormat == 'Bluebook'">
+Bluebook Chapter
+</p>
+
+<p v-else-if="art.type == 'chapter' && citeFormat == 'APA'">
+APA Chapter
+</p>
+
+<p v-else-if="art.type == 'chapter' && citeFormat == 'MLA'">
+MLA Chapter
+</p>
+
+<p v-else>
+Sorry, I don't have a clear citation rule for this item.
+</p>
+
+<p>Change citation format:</p>
+
+<select v-model="citeFormat">
+  <option>Chicago</option>
+  <option>Bluebook</option>
+  <option>APA</option>
+  <option>MLA</option>
+  </select>
+
+
+</div>
+</template>
+
+<script>
+
+ export default {
+     props: ["art"],
+     data: function(){return {citeFormat: "Chicago"};},
+     methods: {authorMaker: (coau) => coau.split(" ").reverse().join(", ") + ", and Paul Gowder."}
+     }
+
+// AUTHORMAKER DOES NOT HANDLE JOURNAL ON LEGISLATION GROUP AUTHORSHIP CASE.
+
+</script>
