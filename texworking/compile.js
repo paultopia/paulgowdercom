27e42a3108f3cee2a2fafd27422d596f0ee15a67
Paul Gowder <paul.gowder@gmail.com>
2017-06-11 14:39:35 -0500

    complex node cv build works

--- texworking/compile.js
+++ texworking/compile.js
@@ -1,9 +1,11 @@
 const latex = require('node-latex');
 const fs = require('fs')
 
-const input = fs.createReadStream('boxes.tex')
+const input = fs.createReadStream('./blue/resume.tex')
 const output = fs.createWriteStream('output.pdf')
-
+const opts = {cmd: 'xelatex', 
+	inputs: './blue',
+        fonts: './blue'}
 latex(input).pipe(output)
 
 
