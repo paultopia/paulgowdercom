73b83161245eb37c9d6457d6a8173b25bbfa5e81
Paul Gowder <paul.gowder@gmail.com>
2017-06-11 15:20:28 -0500

    mustache templates work with tex

--- texworking/compile.js
+++ texworking/compile.js
@@ -1,11 +1,22 @@
 const latex = require('node-latex');
 const fs = require('fs')
+const Mustache = require('mustache')
 
-const input = fs.createReadStream('./blue/resume.tex')
+const customTags = [ '<<', '>>' ];
+Mustache.tags = customTags;
+
+const templatedata = {name: "P. Gowder"};
+
+const template = fs.readFileSync("./working/template.tex", "utf8")
+
+const input = Mustache.render(template, templatedata);
+
 const output = fs.createWriteStream('output.pdf')
-const opts = {cmd: 'xelatex', 
-	inputs: './blue',
-        fonts: './blue'}
+
+const opts = {cmd: 'xelatex',
+	            inputs: './working',
+              fonts: './working'}
+
 latex(input).pipe(output)
 
 
